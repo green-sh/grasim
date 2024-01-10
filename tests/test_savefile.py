3ceb3edec76468d591bfc633054be84a5999462c
greenishhhh <playgreenxst@gmail.com>
2024-01-10 14:36:56 +0100

    added tests and resolve #3

--- tests/test_savefile.py
+++ tests/test_savefile.py
@@ -30,3 +30,14 @@ END B
         """.split("\n")
     )
     assert (graph.graph_matrix == np.array([[0, 0], [1, 0]])).all()
+
+def test_minimal_directed_seperate_double():
+    graph = parse_text(
+        """
+A <-2- B
+A -1-> B
+START A
+END B
+        """.split("\n")
+    )
+    assert (graph.graph_matrix == np.array([[0, 2], [1, 0]])).all()
